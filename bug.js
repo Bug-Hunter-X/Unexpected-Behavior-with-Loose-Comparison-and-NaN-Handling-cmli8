6174e44e3ebab09b3b5373109a83d463f0c2fe35
BugHunter-X <askzenai@gmail.com>
2025-02-18 02:26:06 +0100

    Add bug.js file

---
+++ bug.js
@@ -0,0 +1,14 @@
+function foo(x) {
+  if (x === null) {
+    return 0;
+  } else if (x > 10) {
+    return 2 * x;
+  } else {
+    return x + 1;
+  }
+}
+
+console.log(foo(null)); // 0
+console.log(foo(12)); // 24
+console.log(foo(5)); // 6
+console.log(foo(NaN)); //NaN, should return 0 because it's not a number
